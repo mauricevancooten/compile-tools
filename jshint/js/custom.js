e3f4958e625416ec768ef8b5a0c07d8d65203a73
Maurice van Cooten <maurice@minus.nz>
2016-05-27 10:52:48 +1200

    example using jshint

---
+++ jshint/js/custom.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', unction () {
+			var button = document.querySelector('.show'),
+			 text = document.querySelector('.enquiries'),
+			 visible = false;
+
+			 text.classList.add('accessibly-hidden');
+
+			 button.addEventListener('click', function () {
+
+			 	if ( visible ) {
+
+			 		text.classList.add('accessibly-hidden');
+
+			 	} else {
+
+			 		text.classList.remove('accessibly-hidden');
+			 	}
+
+			 	visible = ! visible;
+
+			 });
+		});
